0dafe223870beb052104fab6e4d149e39b86562b
CandraDedySetiawan <24523056@students.uii.ac.id.com>
2025-12-19 00:59:08 +0700

    Update

--- php/config.php
+++ php/config.php
@@ -1,14 +1,16 @@
 <?php
-// config.php
-$host = "localhost";      // sesuaikan
-$db   = "database"; // sesuaikan
-$user = "root";           // sesuaikan
-$pass = "root";               // sesuaikan
+
+
+if (file_exists(__DIR__ . '/credentials.php')) {
+    include(__DIR__ . '/credentials.php');
+} else {
+    die("Error: File credentials.php tidak ditemukan! Silakan buat file ini sesuai settingan komputer masing-masing.");
+}
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("Koneksi Database Gagal: " . $e->getMessage());
 }
 ?>
