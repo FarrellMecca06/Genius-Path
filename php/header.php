59a6bc8e69103c1a2e1a17307b0b973cca25b7cf
FarrellMecca06 <24523039@students.uii.ac.id>
2026-01-06 09:06:03 +0700

    p

--- php/header.php
+++ php/header.php
@@ -16,26 +16,37 @@ $userName = $_SESSION['user_full_name'] ?? null;
 
 <nav class="navbar">
     <div class="nav-left">
+        <?php if ($userName): ?>
+            <?php 
+            $user_id = $_SESSION['user_id'];
+            include_once __DIR__ . '/config.php';
+            $stmt = $pdo->prepare("SELECT profile_picture FROM users WHERE id = ?");
+            $stmt->execute([$user_id]);
+            $userData = $stmt->fetch();
+            $img_path = $userData['profile_picture'] ? get_template_directory_uri().'/../image/uploads/'.$userData['profile_picture'] : get_template_directory_uri().'/../image/default-user.png';
+            ?>
+            <a href="<?php echo home_url('/profile.php'); ?>" class="nav-profile-link">
+                <img src="<?php echo $img_path; ?>" alt="Profile" class="nav-avatar">
+            </a>
+        <?php endif; ?>
+
         <a href="<?php echo home_url(); ?>" class="logo">
             <img src="<?php echo get_template_directory_uri(); ?>/../image/logo.png" alt="GeniusPath Logo" class="nav-logo-img">
         </a>
+        
         <ul class="nav-links">
             <li><a href="<?php echo home_url(); ?>">Home</a></li>
             <li><a href="<?php echo home_url('/self_discovery.php'); ?>">Self Discovery</a></li>
             <li><a href="<?php echo home_url('/careers.php'); ?>">Career Paths</a></li>
-            <?php if ($userName): ?>
-                <li><a href="<?php echo home_url('/profile.php'); ?>">Profile</a></li>
-                <li><a href="<?php echo home_url('/logout.php'); ?>" class="btn-outline">Logout</a></li>
-            <?php endif; ?>
-            <?php if (!isset($_SESSION['user_id'])): ?>
-                <li><a href="<?php echo home_url('/login.php'); ?>" class="btn-outline">Login</a></li>
-                <li><a href="<?php echo home_url('/register.php'); ?>" class="btn-primary">Get Started</a></li>
-            <?php endif; ?>
         </ul>
     </div>
     <div class="nav-right">
         <?php if ($userName): ?>
             <span class="user-pill">Hi, <?php echo htmlspecialchars($userName); ?></span>
+            <a href="<?php echo home_url('/logout.php'); ?>" class="btn-outline">Logout</a>
+        <?php else: ?>
+            <a href="<?php echo home_url('/login.php'); ?>" class="btn-outline">Login</a>
+            <a href="<?php echo home_url('/register.php'); ?>" class="btn-primary">Get Started</a>
         <?php endif; ?>
     </div>
 </nav>
